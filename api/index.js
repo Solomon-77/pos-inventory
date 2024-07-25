2ae817d0ac93ebf606a1e05b5b78d807fb52c045
Eustaquio <cyrilleustaquio111222@gmail.com>
2024-07-25 13:35:23 +0800

    transfer

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ app.use(cors({
 app.use(express.json());
 
 app.get("/", (req, res) => {
-   res.send("Server is running.");
+   res.send("Server is running.")
 });
 
 app.get("/keep-warm", (req, res) => {
